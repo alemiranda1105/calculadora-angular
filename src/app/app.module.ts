5f139197b8c1bcf68090eb8da63cab0f8e09a888
Alejandro Miranda <amiranda110500@gmail.com>
2022-03-22 09:47:13 +0000

    Diseño inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponentComponent } from './calculator-component/calculator-component.component';
+import { ResultComponentComponent } from './result-component/result-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponentComponent,
+    ResultComponentComponent
   ],
   imports: [
     BrowserModule
